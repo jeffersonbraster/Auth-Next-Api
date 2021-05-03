8308d8a6ac740375de30d9ddfd8f6c57c4a9f561
Jefferson <jeffersonbraster@gmail.com>
2021-05-03 16:04:29 -0300

    configurando axios e fazendo chamada pra api no contexto

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,4 +1,5 @@
 import React, { createContext } from "react";
+import { api } from "../services/api";
 
 type SignInCredentials = {
   email: string;
@@ -20,7 +21,13 @@ export function AuthProvider({ children }: AuthProviderProps) {
   const isAuthenticated = false;
 
   async function signIn({ email, password }: SignInCredentials) {
-    console.log({ email, password });
+    try {
+      const response = await api.post("sessions", { email, password });
+
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
