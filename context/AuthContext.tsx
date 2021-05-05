d0b58a1c1aab196c2a8787e86c48c1f82149d27d
Jefferson <jeffersonbraster@gmail.com>
2021-05-05 20:51:12 -0300

    broadcast de logout

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -15,7 +15,8 @@ type SignInCredentials = {
 };
 
 type AuthContextData = {
-  signIn(credentials: SignInCredentials): Promise<void>;
+  signIn: (credentials: SignInCredentials) => Promise<void>;
+  signOut: () => void;
   user: User;
   isAuthenticated: boolean;
 };
@@ -26,9 +27,13 @@ type AuthProviderProps = {
   children: React.ReactNode;
 };
 
+let authChannel: BroadcastChannel;
+
 export function signOut() {
   destroyCookie(undefined, "nextauth-token");
   destroyCookie(undefined, "nextauth-refreshtoken");
+
+  authChannel.postMessage("signOut");
 
   Router.push("/");
 }
@@ -36,6 +41,20 @@ export function signOut() {
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>(null);
   const isAuthenticated = !!user;
+
+  useEffect(() => {
+    authChannel = new BroadcastChannel("auth");
+
+    authChannel.onmessage = (message) => {
+      switch (message.data) {
+        case "signOut":
+          signOut();
+          break;
+        default:
+          break;
+      }
+    };
+  }, []);
 
   useEffect(() => {
     const { "nextauth-token": token } = parseCookies();
@@ -85,7 +104,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
   }
 
   return (
-    <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
+    <AuthContext.Provider value={{ signIn, isAuthenticated, user, signOut }}>
       {children}
     </AuthContext.Provider>
   );
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,17 +1,17 @@
 import { useContext } from "react";
 import { Can } from "../components/can";
 import { AuthContext } from "../context/AuthContext";
-import { useCan } from "../hooks/useCan";
 
 import { setupApiClient } from "../services/api";
 import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
-  const { user } = useContext(AuthContext);
+  const { user, signOut } = useContext(AuthContext);
 
   return (
     <div>
       <h1>dashboard: {user?.email}</h1>
+      <button onClick={signOut}>Sair</button>
 
       <Can permissions={["metrics-list"]}>
         <div>Métricas</div>
